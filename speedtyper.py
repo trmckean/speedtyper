ab575867a452ae4873d7ac0904dd5ebfef326ebe
trmckean <tyler.r.mckean@intel.com>
2017-07-05 15:51:29 -0700

    Adding better user instructions

--- speedtyper.py
+++ speedtyper.py
@@ -1,26 +1,31 @@
 import time
 
+print "Hello and welcome to SpeedTyper!"
+ready = False
+
+while not ready:
+	userReady = raw_input("Please type 'ready' when you want to begin\n")
+	ready = (userReady == 'ready')
+
+
 test = "Hello, my name is Tyler McKean."
 words = len(test.split())
 
+print "Get ready to type in the phrase when it appears in 5 seconds!"
+
+for i in range(5):
+	print 5 - i
+	time.sleep(1)
 
 starttime = time.time()
-
-user_type = raw_input(test + "\n")
-
+user_type = raw_input(test + "\n" + "Your input: \n")
 endtime = time.time()
-
 
 elapsedTime = endtime - starttime
 elapsedString = "{0:.2f}".format(elapsedTime)
-wpm = (words/(elapsedTime/60))
-
-
-
+wpm = "{0:.2f}".format(words/(elapsedTime/60))
 
 if user_type == test:
-
-	print "You typed the phrase in {0} seconds and at a speed of {1} WPM!".format(elapsedString, wpm)
-
+	print "You typed the phrase in {0} seconds at a speed of {1} WPM!".format(elapsedString, wpm)
 else:
 	print "You failed to type the correct phrase!"
